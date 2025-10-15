1e29dec751096401fe63e91edd7f6ce31def8ca4
dongju <jinwoo060123@gmail.com>
2025-10-15 20:53:56 +0900

    check

--- packages/signal/tools/tool.py
+++
@@ -1,6 +0,0 @@
-from scripts.tool import ToolBase
-from aiohttp import web
-
-class Signal(ToolBase):
-    def load(self, request):
-        return web.FileResponse("/packages/signal/assets/tool-script.js")
